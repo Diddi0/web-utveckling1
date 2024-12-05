febf3822b25e9393b735c5d9c107b0c810d570c5
Diddi0 <mima0920@storsjogymnasiet.se>
2024-12-05 11:54:06 +0100

    egg1

---
+++ gmail maker/script.js
@@ -0,0 +1,16 @@
+let input1 = document.getElementById("input1")
+let input2 = document.getElementById("input2")
+let result = document.getElementById("result")
+
+function createMail(){
+    result.innerHTML = input1.value + "." + input2.value + "@mail.com"
+
+}
+
+
+function randomfärg(){
+const r = math.floor(math.random() * 256)
+const g = math.floor(math.random() * 256)
+const b = math.floor(math.random() * 256)
+}
+
