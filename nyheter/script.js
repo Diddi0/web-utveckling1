e1ae700cdeed50607c6bf6b825c0f9986beaffe3
Diddi0 <mima0920@storsjogymnasiet.se>
2025-04-14 12:15:29 +0200

    "THE" JOE ANDERSON
    g

---
+++ nyheter/script.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click', () => {
+    alert('Redirecting to shop...');
+    window.location.href = "http://127.0.0.1:5500/ad-maker/Milf.html"
+    // You can add code here to redirect to a shopping page
+});
